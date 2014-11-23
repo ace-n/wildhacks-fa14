35ac7a1ef96847024e55abff76658f98eff223b5
Ace Nassri <ace.nassri@gmail.com>
2014-11-22 22:59:52 -0600

    fsck it ship it

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+"""
+HTTP Pages
+"""
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
