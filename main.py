48293f8988ef8e95bcd74a4928de434c664efc50
Ace Nassri <ace.nassri@gmail.com>
2014-11-23 03:55:33 -0600

    wip

--- main.py
+++ main.py
@@ -9,5 +9,17 @@ HTTP Pages
 def home():
 	return render_template('home.html')
 
+@app.route('/chat/')
+def chat():
+	return render_template('chat.html')
+
+@app.route('/login/')
+def login():
+	return render_template('login.html')
+
+@app.route('/register/')
+def register():
+	return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
